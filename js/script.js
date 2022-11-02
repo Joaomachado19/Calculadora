b419b4b504a3735974c68826904d5fe168990eb5
Joaomachado <joaovwmachado@gmail.com>
2022-11-02 13:15:08 -0300

    Atualização 2
    Já adicionado o js faltando o fazer a virgula funcionar

---
+++ js/script.js
@@ -0,0 +1,133 @@
+const numeros = document.querySelectorAll(".numero");
+const operadores = document.querySelectorAll(".operador");
+const calcular = document.querySelector("#igual");
+const limpar = document.querySelector("#clear");
+const currentOperandTextElement = document.querySelector(".numeroAtual");
+const previousOperandTextElement = document.querySelector(".numeroAnterior");
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  formatDisplayNumber(number) {
+    const stringNumber = number.toString();
+
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  calculate() {
+    let result;
+
+    const _previousOperand = parseFloat(this.previousOperand);
+    const _currentOperand = parseFloat(this.currentOperand);
+
+    if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+    switch (this.operation) {
+      case "+":
+        result = _previousOperand + _currentOperand;
+        break;
+      case "-":
+        result = _previousOperand - _currentOperand;
+        break;
+      case "÷":
+        result = _previousOperand / _currentOperand;
+        break;
+      case "*":
+        result = _previousOperand * _currentOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+
+    if (this.previousOperand !== "") {
+      this.calculate();
+    }
+
+    this.operation = operation;
+
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  appendNumber(number) {
+    if (this.currentOperand.includes(".") && number === ".") return;
+
+    this.currentOperand = `${this.currentOperand}${number.toString()}`;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  updateDisplay() {
+    this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
+      this.previousOperand
+    )} ${this.operation || ""}`;
+    this.currentOperandTextElement.innerText = this.formatDisplayNumber(
+      this.currentOperand
+    );
+  }
+}
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+for (const numberButton of numeros) {
+  numberButton.addEventListener("click", () => {
+    calculator.appendNumber(numberButton.innerText);
+    calculator.updateDisplay();
+  });
+}
+
+for (const operationButton of operadores) {
+  operationButton.addEventListener("click", () => {
+    calculator.chooseOperation(operationButton.innerText);
+    calculator.updateDisplay();
+  });
+}
+
+limpar.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+calcular.addEventListener("click", () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+});
